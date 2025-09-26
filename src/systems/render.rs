fccd307eb8db970297c4f5f0945a675c6737faf3
annie <annabanana1701d@gmail.com>
2025-09-26 08:24:43 -0700

    add resource bars

--- src/systems/render.rs
+++ src/systems/render.rs
@@ -3,6 +3,41 @@ use bevy::prelude::*;
 use bevy_ascii_terminal::string::TerminalString;
 use bevy_ascii_terminal::*;
 
+pub fn draw_resource_bar(
+    mut terminal_query: &mut Query<&mut Terminal>, // a query to the terminal resource
+    resource_name: &str,                           // name of the resource (e.g., "HP", "XP", "MANA")
+    filled_char: char,                             // character to represent filled portion of the resource bar
+    bar_length: usize,                             // total length of the resource bar
+    current_value: usize,                          // current value of the resource
+    max_value: usize,                              // maximum value of the resource pool
+    bar_color: Color,                              // color
+    bar_x_position: usize,                         // placeholder: x position of the bar (not used in future implementation)
+) {
+    if let Ok(mut terminal) = terminal_query.single_mut() {
+        let resource_ratio = current_value / max_value;
+        let filled_length = (resource_ratio as f32 * bar_length as f32) as usize;
+
+        let mut bar_content = String::new();
+        for i in 0..bar_length {
+            if i < filled_length {
+                bar_content.push(filled_char);
+            }
+        }
+
+        let mut bar_ts = TerminalString::from(bar_content);
+        bar_ts.decoration.fg_color = Some(LinearRgba::from(bar_color));
+
+        // position the bar in the top-left corner, ensuring it fits
+        let formatted_resource_name = format!("{}:", resource_name);
+        let formatted_name_length = formatted_resource_name.len();
+        let formatted_bar_position = bar_x_position + formatted_name_length;
+        if bar_length + formatted_bar_position <= terminal.size()[0] as usize {
+            terminal.put_string([bar_x_position, 0], formatted_resource_name);
+            terminal.put_string([formatted_bar_position, 0], bar_ts);
+        }
+    }
+}
+
 pub fn draw_scene(
     player_query: Query<&Player>,
     enemy_query: Query<&Enemy>,
@@ -45,30 +80,9 @@ pub fn draw_scene(
             }
         }
 
-        // draw player info(hp bar, etc)
-        if let Ok(player) = player_query.single() {
-            let health_ratio = player.health / player.max_health;
-            let bar_length = 20;
-            let filled_length = (health_ratio * bar_length as f32) as usize;
-
-            let healthbar_base = String::from("HP:");
-            let mut healthbar_content = String::new();
-            for i in 0..bar_length {
-                if i < filled_length {
-                    healthbar_content.push('#');
-                } else {
-                    healthbar_content.push('-');
-                }
-            }
-
-            let mut healthbar_ts = TerminalString::from(healthbar_content);
-            healthbar_ts.decoration.fg_color = Some(Color::linear_rgb(0.0, 1.0, 0.0).into());
-
-            // position the healthbar in the top-left corner, ensuring it fits
-            if bar_length + 6 <= terminal.size()[0] as usize {
-                terminal.put_string([0, 0], healthbar_base);
-                terminal.put_string([4, 0], healthbar_ts);
-            }
-        }
+        // draw player info(hp bar, xp, etc)
+        let player_query = player_query.single().unwrap();
+        draw_resource_bar(&mut terminal_query, "HP", '#', 20, player_query.health as usize, player_query.max_health as usize, Color::linear_rgba(0.0, 1.0, 0.1, 1.0), 0);
+        draw_resource_bar(&mut terminal_query, "XP", '#', 20, 30, 100, Color::linear_rgba(0.1, 0.25, 1.0, 1.0), 30);
     }
 }
