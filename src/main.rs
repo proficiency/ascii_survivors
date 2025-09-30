e897fad4c9fcb74865d7c2c2e9e4d6eb02162fcb
annie <annabanana1701d@gmail.com>
2025-09-30 01:30:48 -0700

    refactor `main.rs` formatting

--- src/main.rs
+++ src/main.rs
@@ -210,7 +210,9 @@ fn reset_fade_timer(mut fade_timer: ResMut<FadeTimer>) {
 }
 
 fn play_start_sound(mut sound_manager: ResMut<SoundManager>) {
-    sound_manager.play_sound(PathBuf::from("./start.wav"), -5.0).expect("Failed to play start sound");
+    sound_manager
+        .play_sound(PathBuf::from("./start.wav"), -5.0)
+        .expect("Failed to play start sound");
 }
 
 fn fade_in_render_system(mut query: Query<&mut Terminal>, fade_timer: Res<FadeTimer>) {
