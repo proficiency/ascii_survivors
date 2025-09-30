8a83772ed5bc0503c9c0fa08795908f6b011f000
Kasullian <tomkasull@gmail.com>
2025-09-29 21:32:21 -0400

    update input for player

--- src/systems/player_movement.rs
+++ src/systems/player_movement.rs
@@ -25,38 +25,49 @@ pub fn player_movement(
         let mut move_offset = IVec2::new(0, 0);
         for (_, gamepad) in &gamepad_input {
             let left_stick = gamepad.left_stick();
-
-            // print!("Left Stick: x: {}, y: {}\r", left_stick.x, left_stick.y);
-            // print!(
-            //     "Left Stick abs: x: {}, y: {}\r",
-            //     left_stick.x.abs(),
-            //     left_stick.y.abs()
-            // );
-
             const TOLERANCE: f32 = 0.35f32;
+
             if left_stick.y < -TOLERANCE {
                 move_offset.y -= 1;
-            } else if left_stick.y > TOLERANCE {
+            } 
+            if left_stick.y > TOLERANCE {
                 move_offset.y += 1;
-            } else if left_stick.x < -TOLERANCE {
+            } 
+            if left_stick.x < -TOLERANCE {
                 move_offset.x -= 1;
-            } else if left_stick.x > TOLERANCE {
+            } 
+            if left_stick.x > TOLERANCE {
+                move_offset.x += 1;
+            }
+
+            // D-Pad movement
+            if gamepad.pressed(GamepadButton::DPadUp) {
+                move_offset.y += 1;
+            }
+            if gamepad.pressed(GamepadButton::DPadDown) {
+                move_offset.y -= 1;
+            }
+            if gamepad.pressed(GamepadButton::DPadLeft) {
+                move_offset.x -= 1;
+            }
+            if gamepad.pressed(GamepadButton::DPadRight) {
                 move_offset.x += 1;
             }
         }
 
-        if keyboard_input.pressed(KeyCode::KeyW) {
+        if keyboard_input.pressed(KeyCode::KeyW) || keyboard_input.pressed(KeyCode::ArrowUp) {
             move_offset.y += 1;
         }
-        if keyboard_input.pressed(KeyCode::KeyS) {
+        if keyboard_input.pressed(KeyCode::KeyS) || keyboard_input.pressed(KeyCode::ArrowDown) {
             move_offset.y -= 1;
         }
-        if keyboard_input.pressed(KeyCode::KeyA) {
+        if keyboard_input.pressed(KeyCode::KeyA) || keyboard_input.pressed(KeyCode::ArrowLeft) {
             move_offset.x -= 1;
         }
-        if keyboard_input.pressed(KeyCode::KeyD) {
+        if keyboard_input.pressed(KeyCode::KeyD) || keyboard_input.pressed(KeyCode::ArrowRight) {
             move_offset.x += 1;
         }
+        
         // this is kinda weird
         camera_offset.0 -= move_offset.clamp(IVec2::new(-1, -1), IVec2::new(1, 1));
         player.position = IVec2::new(center_x, center_y);
